f2009060dfa31b6f52b3f3b254493f8df2de675a
Brijesh1990 <bkpandey.pandey@gmail.com>
2022-12-17 18:46:37 +0530

    Add files via upload

---
+++ add-app/src/Add.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const a=20;
+const b=30;
+function Add()
+{
+    return(
+        <React.Fragment>
+            <h1>Additions of Numbers</h1>
+            <h3>Additions of Numebrs :{a+b}</h3>
+        </React.Fragment>
+
+    );
+}
+export default Add;
---
+++ jsx-app/src/components/AboutUs.jsx
@@ -0,0 +1,15 @@
+function AboutUs()
+{
+    return (
+
+         <>
+               <h1 align="center">About Us</h1>
+               <p align="center"> lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem<br/> ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum</p>
+
+               <h5 align="center">Copyright @2022 all right reserved</h5>
+         </>
+
+    )
+}
+
+export default AboutUs;
