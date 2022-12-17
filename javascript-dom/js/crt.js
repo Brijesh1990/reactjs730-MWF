8b5baa0a2e695c5d9f3afbd90eb4c6775cb2df79
Brijesh1990 <bkpandey.pandey@gmail.com>
2022-12-17 18:48:50 +0530

    Add files via upload

---
+++ javascript-dom/js/crt.js
@@ -0,0 +1,7 @@
+function carttotal()
+{
+    var p=document.getElementById("price").value;
+    var q=document.getElementById("qty").value;
+    var t=p*q;
+    document.getElementById("tot").innerHTML=t;
+}
