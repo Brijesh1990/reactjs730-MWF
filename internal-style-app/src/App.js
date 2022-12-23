f419c6cb263966e5efc75f1995bf7c0d072b2e75
Brijesh1990 <bkpandey.pandey@gmail.com>
2022-12-23 10:44:24 +0530

    Add files via upload

---
+++ internal-style-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="content">
+        <div className='leftsidebar'></div>    
+        <div className='rightsidebar'></div>
+     </div>
+  );
+}
+
+export default App;
